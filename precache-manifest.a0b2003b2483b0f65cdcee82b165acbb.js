2cf6ca762a0f3d3aee30f789a2762700c6b5f891
bmai <bmai@sfu.ca>
2020-07-30 15:55:39 -0700

    Updates

---
+++ precache-manifest.a0b2003b2483b0f65cdcee82b165acbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d955eeb37624fe6a4eeb63fca2268266",
+    "url": "/budget-tracker/index.html"
+  },
+  {
+    "revision": "aec8634bd6b7b67daaa1",
+    "url": "/budget-tracker/static/js/2.90c46c29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/budget-tracker/static/js/2.90c46c29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9d196542080a8815c6",
+    "url": "/budget-tracker/static/js/main.6b75ad84.chunk.js"
+  },
+  {
+    "revision": "bc515d496ebdae6bf221",
+    "url": "/budget-tracker/static/js/runtime-main.9010eaf8.js"
+  }
+]);
